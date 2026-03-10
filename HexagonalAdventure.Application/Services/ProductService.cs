0476da0b524a52abd098f87dc0ec7e7f9930f0ae
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-03-10 11:08:16 +0300

    step#04 Implement ProductService to manage product creation and interaction with IProductRepository as a application service.

---
+++ HexagonalAdventure.Application/Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using HexagonalAdventure.Application.Ports.Outbound;
+using HexagonalAdventure.Domain;
+
+namespace HexagonalAdventure.Application.Services;
+
+public class ProductService(IProductRepository productRepository)
+{
+    private readonly IProductRepository _productRepository = productRepository;
+
+    public Guid CreateProduct(string title, decimal price, string category, int stock)
+    {
+        // Domain nesnesi oluşturulur ve orada tanımlı iş kuralları da yürütülür.
+        var product = new Product(Guid.NewGuid(), title, price, category, stock);
+        // Outbound port olarak tanımladığımız arayüz üzerinden ürün ekleme işlevi çağırılır
+        _productRepository.AddProduct(product);
+        return product.Id;
+    }
+}
