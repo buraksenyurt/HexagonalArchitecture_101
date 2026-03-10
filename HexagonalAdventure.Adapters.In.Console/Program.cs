b742304077e7c4a89d34c4ac2bdc3711c183a0ac
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-03-10 18:08:22 +0300

    step#08 Add console adapter project and implement product creation functionality

---
+++ HexagonalAdventure.Adapters.In.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using HexagonalAdventure.Adapters.Out.InMemory;
+using HexagonalAdventure.Application.Ports.Inbound;
+using HexagonalAdventure.Application.Ports.Outbound;
+using HexagonalAdventure.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<IProductRepository, InMemoryProdutRepository>()
+    .AddScoped<IProductService, ProductService>()
+    .BuildServiceProvider();
+
+Console.WriteLine("Add a new product");
+
+Console.Write("Title: ");
+string title = Console.ReadLine();
+
+Console.Write("Price: ");
+decimal price = decimal.Parse(Console.ReadLine());
+
+Console.Write("Category: ");
+string category = Console.ReadLine();
+
+Console.Write("Stock: ");
+int stock = int.Parse(Console.ReadLine());
+
+var productService = serviceProvider.GetRequiredService<IProductService>();
+var newProductId = productService.CreateProduct(title, price, category, stock);
+
+Console.WriteLine("Product created with ID: " + newProductId);
+Console.ReadLine();
