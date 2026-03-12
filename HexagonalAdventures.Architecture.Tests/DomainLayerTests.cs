4adbc6805dcaf079889c9688c482e66e8ac43e8a
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-03-12 18:21:30 +0300

    Add architecture tests for domain and application layers to enforce dependency rules

---
+++ HexagonalAdventures.Architecture.Tests/DomainLayerTests.cs
@@ -0,0 +1,44 @@
+﻿using HexagonalAdventure.Application.Services;
+using HexagonalAdventure.Domain;
+using NetArchTest.Rules;
+
+namespace HexagonalAdventures.Architecture.Tests;
+
+public class DomainLayerTests
+{
+    [Fact]
+    public void DomainLayer_ShouldNotHaveDependencyOnOtherLayers()
+    {
+        // Arrange
+        var domainAssembly = typeof(Product).Assembly;
+
+        // Act
+        var result = Types.InAssembly(domainAssembly)
+            .ShouldNot()
+            .HaveDependencyOnAny(
+            "HexagonalAdventure.Application",
+            "HexagonalAdventure.Adapters",
+            "Microsoft.EntityFrameworkCore"
+            )
+            .GetResult();
+
+        // Assert
+        Assert.True(result.IsSuccessful, "Domain layer should not have dependencies on Application, Adapters, or EF Core.");
+    }
+
+    [Fact]
+    public void ApplicationLayer_ShouldNotHaveDependencyOnAdapters()
+    {
+        // Arrange
+        var appAssembly = typeof(ProductService).Assembly;
+        
+        // Act
+        var result = Types.InAssembly(appAssembly)
+            .ShouldNot()
+            .HaveDependencyOn("HexagonalAdventure.Adapters")
+            .GetResult();
+
+        // Assert
+        Assert.True(result.IsSuccessful, "Application layer should not have dependencies on Adapters.");
+    }
+}
